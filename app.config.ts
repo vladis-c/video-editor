20eb70643d74e598071226d6e71db14fae3d500f
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2023-09-01 20:28:39 +0300

    initial dependencies and app config

---
+++ app.config.ts
@@ -0,0 +1,65 @@
+import {ExpoConfig, ConfigContext} from 'expo/config';
+
+export default ({config}: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'VideoEditor',
+  slug: 'video-editor',
+  owner: 'vladis-c',
+  version: '1.0.0',
+  platforms: ['android', 'ios'],
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  backgroundColor: '#EFEFEF',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#EFEFEF',
+  },
+  assetBundlePatterns: ['**/*'],
+  android: {
+    package: 'com.vladisc.videoeditor',
+    jsEngine: 'hermes',
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#EFEFEF',
+    },
+  },
+  plugins: [
+    [
+      'expo-build-properties',
+      {
+        android: {
+          minSdkVersion: 24,
+          packagingOptions: {
+            jniLibs: {
+              useLegacyPackaging: true,
+            },
+          },
+        },
+      },
+    ],
+    [
+      '@config-plugins/ffmpeg-kit-react-native',
+      {
+        package: 'full-gpl',
+      },
+    ],
+    'expo-screen-orientation',
+    [
+      'expo-media-library',
+      {
+        photosPermission:
+          'Allow $(PRODUCT_NAME) to access your gallery for saving videos.',
+        savePhotosPermission:
+          'Allow $(PRODUCT_NAME) to access your gallery for saving videos.',
+        isAccessMediaLocationEnabled: true,
+      },
+    ],
+  ],
+  extra: {
+    eas: {
+      projectId: '',
+    },
+  },
+});
