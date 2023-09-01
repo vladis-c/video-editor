c5978a87011d9d7ea4e9384205546053d1750f5a
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2023-09-01 20:19:25 +0300

    Initial file config setup

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 'off',
+    'react-native/no-color-literals': 'off',
+  },
+};
